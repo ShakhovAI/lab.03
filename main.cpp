2383f7d6f380ce0223ccb3c73c382506a17c3324
ShakhovAI <shakhov-arthur@mail.ru>
2020-10-04 16:08:58 +0300

    code: добавлен ввод из произвольного потока

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 #include "svg.h"
 using namespace std;
 
-vector<double> input_numbers(size_t count)
+vector<double> input_numbers(istream& in, size_t count)
 {
     vector<double> result(count);
     cerr << "Enter numbers: ";
     for (size_t i = 0; i < count; i++)
     {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
@@ -35,7 +35,7 @@ int main()
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(cin, number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
