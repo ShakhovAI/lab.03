b23509b5dc688bda99d351f510f2623c98e489f0
ShakhovAI <shakhov-arthur@mail.ru>
2020-10-05 01:09:32 +0300

    Revert "code: проверка вывода с помощью printf"
    This reverts commit acd420dd2c41fa832bfecdad4596aff13c602e7c.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <vector>
 #include "histogram.h"
-#include "svg.h"
-#include <cstdio>
 
 using namespace std;
+
 
 vector<double> input_numbers(size_t count)
 {
@@ -32,13 +31,66 @@ vector<size_t> make_histogram(int bin_count, const vector<double>& numbers, doub
     return bins;
 }
 
+
+void svg_begin(double width, double height)
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n" << '\n';
+}
+
+void svg_text(double left, double baseline, string text)
+{
+    cout << "<text x='" << left << "' y='"<<baseline<<"'>"<<text<<"</text>" << '\n';
+}
+
+void svg_end()
+{
+    cout <<'\n'<< "</svg>\n";
+}
+
+void svg_rect(double x, double y, double width, double height, string stroke, string fil){
+  cout << "<rect x='" << x << "' y='" << y <<"' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fil << "' />" << '\n';
+}
+
+void show_histogram_svg(const vector<size_t>& bins)
+{
+    const auto IMAGE_WIDTH = 500;
+    const auto IMAGE_HEIGHT = 400;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 50;
+
+
+
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    int top = 0;
+
+    for (size_t bin : bins) {
+    const double bin_width = BLOCK_WIDTH * bin;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    switch (top){
+    case 0: svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "red");
+        break;
+    case 30: svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "green", "green");
+        break;
+    case 60: svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "blue", "blue");
+        break;
+}
+    top += BIN_HEIGHT;
+    }
+    svg_end();
+}
+
+
 int main()
 {
-    const char* name = "Artur";
-    const char* operating_system = "Arch Linux";
-    size_t age = 19;
-    printf("My name is %s. I'm %d years old.\n I use %s btw...\n", name, age, operating_system);
-    return 0;
 
     size_t number_count;
     cerr << "Enter number count: ";
